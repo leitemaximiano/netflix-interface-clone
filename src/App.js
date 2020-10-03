4f5ef4b1d5ef6bc9833b5fb53338a100f9e24b65
maximiano leite <macio.pb.cg@gmail.com>
2020-10-03 16:55:52 -0300

    feat(App): finish component app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,80 @@
 import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import './App.css';
 
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 export default () => {
 
-  const [ movieList, setMovieList ] = useState([]); 
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
-  useEffect(()=>{
+
+
+  useEffect(() => {
     const loadAll = async () => {
-      let list = await Tmdb.getHomeList();
-      setMovieList(list)
-    } 
+      const list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // pegando a featured
+      const originals = list.filter(i => i.slug === 'originals');
+      const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      const chosen = originals[0].items.results[randomChosen];
+      const choseInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureData(choseInfo);
+    }
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
+
+
+      {
+        featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key) => (
-          <div>
-            <MovieRow />
-          </div>
+          <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+
+      <footer>
+
+        Feito por com <span role="img" aria-label="coração">❤</span> pela B7web <br />
+        direitos de imagem para Netflix <br />
+        dados pegos por no site Themoviedb.org
+
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="loading"></img>
+        </div>
+      }
     </div>
   );
 }
